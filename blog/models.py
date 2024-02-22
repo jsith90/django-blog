ad8b80d6e0d7bd048f655bd43d238ed18bad202c
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-02-22 10:17:10 +0000

    comment model create

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,17 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments" 
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(defaul=False) 
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+
