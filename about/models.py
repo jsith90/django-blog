835cb6e1fa80dd1d80f077011bc0d49aae6d027f
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-02-26 09:42:29 +0000

    add about image feature

--- about/models.py
+++ about/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class About(models.Model):
     title = models.CharField(max_length=200, unique=False)
     slug = models.SlugField(max_length=200, unique=False)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
